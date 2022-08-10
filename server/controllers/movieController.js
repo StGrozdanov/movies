55181572d10eb9fc67430d192ddd10a0165b25ef
StGrozdanov <74171353+StGrozdanov@users.noreply.github.com>
2022-08-10 21:35:57 +0300

    Working delete movie endpoint

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -13,15 +13,26 @@ router.post('/', async (request, response) => {
         const createdMovie = await movieService.createMovie(request.body);
         response.status(201).json(createdMovie);
     } catch (error) {
-        console.log(`Error happended durring movie creation: ${error}`);
-        let errorMessage = handleUniqueConstraint(errorMapper(error));
-        response.status(400).json({ errorMessage });
+        console.log(`Error happened durring movie creation: ${error}`);
+        let errorMessage = handleUniqueConstraintError(errorMapper(error));
+        response.status(400).json({ message: errorMessage });
     }
 });
 
-function handleUniqueConstraint(errorMessage) {
+router.delete('/:id', async (request, response) => {
+    const movieId = request.params.id;
+    try {
+        await movieService.deleteMovie(movieId);
+        response.json({});
+    } catch(error) {
+        console.log(`Error happened while trying to delete movie with id ${movieId}: ${error}`);
+        response.status(404).json({ message: `There is no movie with id ${movieId} in the database.` });
+    }
+});
+
+function handleUniqueConstraintError(errorMessage) {
     if (errorMessage.includes('duplicate')) {
-        errorMessage = 'There is already a movie with the same title.'
+        errorMessage = 'There is already a movie with the same title.';
     }
     return errorMessage;
 }
--- server/services/movieService.js
+++ server/services/movieService.js
@@ -2,9 +2,12 @@ const Movie = require('../models/Movie');
 
 const getAllMovies = () => Movie.find({});
 
-const createMovie = (requestBody) => Movie.create(requestBody) 
+const createMovie = (requestBody) => Movie.create(requestBody);
+
+const deleteMovie = (movieId) => Movie.findByIdAndDelete(movieId);
 
 module.exports = {
     getAllMovies,
     createMovie,
+    deleteMovie,
 };
