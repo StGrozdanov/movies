3f0267de6b87e0606aeb6b1ae846e67af0f14207
StGrozdanov <74171353+StGrozdanov@users.noreply.github.com>
2022-08-11 13:44:39 +0300

    Working user registration endpoint with bcrypt and jwt

--- server/controllers/authenticationController.js
+++ server/controllers/authenticationController.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+
+const errorMapper = require('../utils/errorMapper');
 const authenticationService = require('../services/authenticationService');
 
 router.post('/register', async (request, response) => {
@@ -6,8 +8,17 @@ router.post('/register', async (request, response) => {
         const registeredUser = await authenticationService.register(request.body);
         response.json(registeredUser);
     } catch (error) {
-        response.status(400).json({ message: error });
+        console.log(`Error happened durring user registration: ${error}`);
+        let errorMessage = handleUniqueConstraintError(errorMapper(error));
+        response.status(400).json({ message: errorMessage });
     }
 });
+
+function handleUniqueConstraintError(errorMessage) {
+    if (errorMessage.includes('duplicate')) {
+        errorMessage = 'There is already a user with the same username.';
+    }
+    return errorMessage;
+}
 
 module.exports = router;
--- server/services/authenticationService.js
+++ server/services/authenticationService.js
@@ -1,8 +1,37 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 const User = require('../models/User');
 
-const register = ({ username, password }) => User.create({ username, password });
+const register = async ({ username, password }) => {
+    password = password.toString();
+
+    if (password.trim().length < 5) {
+        throw new Error('Minimum password length is 5 symbols long.');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, Number(process.env.SALT_ROUNDS));
+
+    try {
+        const user = await User.create({ username, password: hashedPassword });
+        return createSession(user);
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+function createSession(user) {
+    const payload = { username: user.username, _id: user._id };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
+
+    return {
+        username: user.username,
+        sessionToken: token,
+        _id: user._id,
+    };
+};
 
 module.exports = {
     register,
-    
-}
+};
