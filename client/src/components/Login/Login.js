aeb77a8edd6862c07cd626ef2b862d42032146c8
StGrozdanov <74171353+StGrozdanov@users.noreply.github.com>
2022-08-12 10:37:04 +0300

    Added form validations

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -9,14 +9,14 @@ function Login() {
             <Form>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="email" placeholder="Enter username.." />
+                    <Form.Control type="text" placeholder="Username.." />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password.." />
                 </Form.Group>
-                <Button variant="primary" type="submit">Submit</Button>
+                <Button variant="primary" type="submit">Login</Button>
             </Form>
         </div>
     )
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,27 +1,65 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import styles from './Register.module.css';
 
 function Register() {
+    const [password, setPassword] = useState('');
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        setValidated(true);
+    };
+
     return (
         <div className={styles.container}>
             <h3 style={{ textAlign: "center" }}>Register</h3>
-            <Form>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="email" placeholder="Enter username.." />
+                    <Form.Control type="text" placeholder="Username.." required minLength={3} />
+                    <Form.Control.Feedback type="invalid">
+                        Usernames should be at least 3 characters long.
+                    </Form.Control.Feedback>
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control
+                        type="password"
+                        placeholder="Password.."
+                        required
+                        minLength={5}
+                        onInput={(e) => setPassword(e.target.value)}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        Passwords should be at least 5 characters long.
+                    </Form.Control.Feedback>
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Repeat Password</Form.Label>
-                    <Form.Control type="password" placeholder="Repeat Password" />
+                    <Form.Label>Repeat password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Repeat Password.."
+                        required
+                        minLength={5}
+                        pattern={password}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        Passwords should match.
+                    </Form.Control.Feedback>
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                 </Form.Group>
-                <Button variant="primary" type="submit" >Submit</Button>
+
+                <Button variant="primary" type="submit">Register</Button>
             </Form>
         </div>
     )
