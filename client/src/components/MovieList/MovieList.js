7569a9e26134d06ad8935a8be99779cdb40af1e4
StGrozdanov <74171353+StGrozdanov@users.noreply.github.com>
2022-12-04 20:28:56 +0200

    fixed react child unique key error on homepage

--- client/src/components/MovieList/MovieList.js
+++ client/src/components/MovieList/MovieList.js
@@ -1,14 +1,16 @@
 import MovieCard from "../MovieCard/MovieCard";
 import styles from './Catalogue.module.css';
+import { v4 as uuid } from 'uuid';
 
 function MovieList({ movies, heading, missingMessage }) {
+    
     return (
         <section className={styles['catalogue-section']}>
             <h3 className={styles.heading}>{heading}</h3>
             <article className={styles.container}>
                 {
                     movies.length > 0
-                        ? movies.map(movie => <MovieCard movie={movie} />)
+                        ? movies.map(movie => <MovieCard movie={movie} key={uuid()} />)
                         : missingMessage
                 }
             </article>
