fd9165d36e36e5922fccac4df7c2288b5c71522f
StGrozdanov <74171353+StGrozdanov@users.noreply.github.com>
2022-12-04 19:59:57 +0200

    Movie cards are aligned

--- client/src/components/MovieCard/MovieCard.js
+++ client/src/components/MovieCard/MovieCard.js
@@ -4,18 +4,22 @@ import { NavLink } from 'react-router-dom';
 import styles from './MovieCard.module.css';
 
 function MovieCard({ movie }) {
-    let description = movie.description.length > 90 ? movie.description.substring(0, 91) + '...' : movie.description;
-
+    const summary = summarize(movie.description, 90);
     return (
-        <Card className={styles.card} style={{ width: '16rem' }}>
+        <Card className={styles.card} >
             <Card.Img height={260} variant="top" src={movie.imageUrl} />
             <Card.Body>
-                <Card.Title style={{ textAlign: "center" }}>{movie.title}</Card.Title>
-                <Card.Text>{description}</Card.Text>
+                <Card.Title className={styles.cardHeading}>{movie.title}</Card.Title>
+                <Card.Text>{summary}</Card.Text>
                 <Button as={NavLink} to={`/details/${movie._id}`} variant="primary">Details</Button>
             </Card.Body>
         </Card>
     );
+}
+
+function summarize(property, limit) {
+    return property.length > limit ? property.substring(0, limit) + '...' : property;
+
 }
 
 export default MovieCard;
